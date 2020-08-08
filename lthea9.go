55615ec2883b1c6b396ebaf08d42189dc83fcecf
Adam Richardson <as3richa@uwaterloo.ca>
2020-08-08 15:05:57 -0400

    index builder

--- lthea9.go
+++ lthea9.go
@@ -1,6 +1,7 @@
 package lthea9
 
 import (
+	"bytes"
 	"sort"
 	"strings"
 )
@@ -15,32 +16,28 @@ type SubseqIndexBuilder struct {
 	strs   []bufferString
 }
 
-func (builder *SubseqIndexBuilder) Insert(str string) {
-	builder.strs = append(builder.strs, bufferString{
-		start: builder.buffer.Len(),
-		end:   builder.buffer.Len() + len(str),
-	})
-	builder.buffer.WriteString(str)
+type monogramEntry struct {
+	str int
+	pos byte
 }
 
-func (builder *SubseqIndexBuilder) Build() SubseqIndex {
-	buffer := builder.buffer.String()
-	strs := builder.strs
+type bigramEntry struct {
+	str int
+	pos [2]byte
+}
 
-	lexicographic := make([]int, len(strs))
-	for i := 0; i < len(strs); i += 1 {
-		lexicographic[i] = i
-	}
-	sort.Slice(lexicographic, func(i, j int) bool {
-		left := buffer[strs[i].start:strs[i].end]
-		right := buffer[strs[j].start:strs[j].end]
-		return left < right
-	})
+type SubseqIndex struct {
+	buffer []byte
+	strs   []bufferString
 
-	return SubseqIndex{
-		buffer: buffer,
-		strs:   strs,
-	}
+	monogram [distinctChars][]monogramEntry
+
+	bigram [distinctChars * distinctChars][]bigramEntry
+}
+
+type QueryResult struct {
+	str string
+	pos []byte
 }
 
 const (
@@ -62,28 +59,153 @@ func asciiByteToChar(b byte) byte {
 	}
 }
 
-type SubseqIndex struct {
-	buffer string
-	strs   []bufferString
-
-	lexicographic []int
-
-	monogram [distinctChars][]struct {
-		str   int
-		index byte
-	}
-
-	bigram [distinctChars * distinctChars][]struct {
-		str     int
-		indices [2]byte
-	}
+func bigramId(ch0, ch1 byte) int {
+	return int(ch0)*distinctChars + int(ch1)
 }
 
-type QueryResult struct {
-	str     string
-	matches []byte
+func (builder *SubseqIndexBuilder) Insert(str string) {
+	builder.strs = append(builder.strs, bufferString{
+		start: builder.buffer.Len(),
+		end:   builder.buffer.Len() + len(str),
+	})
+	builder.buffer.WriteString(str)
 }
 
-func (index *SubseqIndex) Query(subseq string) []QueryResult {
-	return nil
+func (builder *SubseqIndexBuilder) Build() SubseqIndex {
+	if len(builder.strs) == 0 {
+		return SubseqIndex{}
+	}
+
+	index := SubseqIndex{
+		buffer: []byte(builder.buffer.String()),
+		strs:   builder.strs,
+	}
+
+	sort.Slice(index.strs, func(str1, str2 int) bool {
+		return bytes.Compare(index.stringBytes(str1), index.stringBytes(str2)) == -1
+	})
+
+	{
+		k := 1
+		for i := 1; i < len(index.strs); i += 1 {
+			if bytes.Equal(index.stringBytes(i), index.stringBytes(i-1)) {
+				continue
+			}
+			index.strs[k] = index.strs[i]
+			k += 1
+		}
+		index.strs = index.strs[:k]
+	}
+
+	for str := range index.strs {
+		var monogramBitmap [(distinctChars + 7) / 8]byte
+
+		for pos, b := range index.stringBytes(str) {
+			ch := asciiByteToChar(b)
+			if bitmapTestAndSet(monogramBitmap[:], int(ch)) {
+				continue
+			}
+
+			index.monogram[ch] = append(index.monogram[ch], monogramEntry{
+				str: str,
+				pos: toByteSaturating(pos),
+			})
+		}
+	}
+	for _, ary := range index.monogram {
+		sort.Slice(ary, func(i, j int) bool {
+			return ary[i].pos < ary[j].pos || (ary[i].pos == ary[j].pos && ary[i].str < ary[j].str)
+		})
+	}
+
+	for str := range index.strs {
+		var bigramBitmap [(distinctChars*distinctChars + 7) / 8]byte
+		strBytes := index.stringBytes(str)
+
+		for pos0, b0 := range strBytes {
+			ch := asciiByteToChar(b0)
+			for pos1, b1 := range strBytes[pos0+1:] {
+				id := bigramId(ch, asciiByteToChar(b1))
+				if bitmapTestAndSet(bigramBitmap[:], id) {
+					continue
+				}
+
+				index.bigram[id] = append(index.bigram[id], bigramEntry{
+					str: str,
+					pos: [2]byte{
+						toByteSaturating(pos0),
+						toByteSaturating(pos1),
+					},
+				})
+			}
+		}
+	}
+	for _, ary := range index.bigram {
+		sort.Slice(ary, func(i, j int) bool {
+			return ary[i].pos[0] < ary[j].pos[0] ||
+				(ary[i].pos[0] == ary[j].pos[0] && ary[i].pos[1] < ary[j].pos[1]) ||
+				(ary[i].pos == ary[j].pos && ary[i].str < ary[j].str)
+		})
+	}
+
+	return index
+}
+
+func (index *SubseqIndex) Query(subseq string, maxResults int, onResult func(QueryResult)) {
+	bytes := []byte(subseq)
+
+	if len(bytes) == 0 {
+		if maxResults > len(index.strs) {
+			maxResults = len(index.strs)
+		}
+		for str := 0; str < maxResults; str += 1 {
+			onResult(QueryResult{
+				str: string(index.stringBytes(str)),
+				pos: nil,
+			})
+		}
+		return
+	}
+
+	if len(bytes) == 1 {
+		ary := index.monogram[asciiByteToChar(bytes[0])]
+		if maxResults > len(ary) {
+			maxResults = len(ary)
+		}
+		for _, entry := range ary {
+			onResult(QueryResult{
+				str: string(index.stringBytes(entry.str)),
+				pos: []byte{entry.pos},
+			})
+		}
+		return
+	}
+
+	chars := make([]byte, len(bytes))
+	for i, b := range bytes {
+		chars[i] = asciiByteToChar(b)
+	}
+
+	// ???
+
+	return
+}
+
+func (index *SubseqIndex) stringBytes(str int) []byte {
+	return index.buffer[index.strs[str].start:index.strs[str].end]
+}
+
+func bitmapTestAndSet(bitmap []byte, k int) bool {
+	if (bitmap[k/8]>>uint(k%8))&1 == 1 {
+		return true
+	}
+	bitmap[k/8] |= 1 << uint(k%8)
+	return false
+}
+
+func toByteSaturating(n int) byte {
+	if n <= 0xff {
+		return byte(n)
+	}
+	return 0xff
 }
